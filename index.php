5ced33eb06154dfc4469fcbb29293d0cfe042cb0
PatsyMorales <pat.sy@live.com>
2014-03-05 10:35:30 -0600

    mi primer commit de muestra para DESARROLLO DE SOFTWARE " Hola mundo en PHP yupi!!!"

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+                
+         echo '<p>Hola Mundo</p>'; 
+         echo '<p>Patsy morales Rodriguez</p>';
+        
+        /*
+        Declaracion de variables 
+                 */
+        $intVar =10;
+        $floatVar =2.58;
+        $stringVar="Patsy Morales Rodriguez";
+        
+        /*Declaracion de Arreglos*/
+        $arrayVar = array(
+            'Boolean',
+            'Integer',
+            'String','Array',
+            'Object',
+            'Resource',
+            'NULL'
+        );
+        
+        $booleanVar = true; // Boolean type
+       
+       
+        ?>
+    </body>
+</html>  
